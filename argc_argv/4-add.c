07bc31f4a12dd7b146a60e555746c66c29a25321
Ryn <Syrine.skl@gmail.com>
2025-11-14 21:36:27 +0000

    fixed

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,50 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "main.h"
-
+#include "holberton.h"
 /**
- * checker - checks if all arguments are positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 if all arguments are valid numbers, 1 if any argument is invalid
- */
-int checker(int argc, char *argv[])
-{
-    int i, j;
-
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (!isdigit(argv[i][j]))
-                return 1;
-        }
-    }
-    return 0;
-}
-
-/**
- * main - adds all positive numbers passed as arguments
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success, 1 if any argument contains a non-digit
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    int i, result = 0;
+	int i, sum = 0;
 
-    if (checker(argc, argv))
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc < 1)
+		return (0);
 
-    for (i = 1; i < argc; i++)
-        result += atoi(argv[i]);
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 
-    printf("%d\n", result);
-    return 0;
+	return (0);
 }
