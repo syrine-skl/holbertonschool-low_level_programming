9ac9f5ce907ee8ea630ffad2440c267a77390390
ryn <syrine.skl@gmail.com>
2026-02-27 14:53:35 +0100

    edit

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -5,14 +5,13 @@ int sum_to_n(int n)
     int i;
     int sum = 0;
 
-    for (i = 1; i < n; i++)
+    for (i = 1; i <= n; i++)
         sum += i;
 
-    return n;
+    return sum;
 }
 
 /* -- DO NOT Modify the code below this line -- */
-
 int main(void)
 {
     printf("%d\n", sum_to_n(10));
