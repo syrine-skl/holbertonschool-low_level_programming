4306ae9982f34e0e21d98c7f83d9a1f6bae5bfe6
Ryn <Ryn@ryn.com>
2026-03-20 22:55:06 +0000

    Lady GAGA

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,14 +3,6 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
-#endif /* FUNCTION_POINTERS_H */
-
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
