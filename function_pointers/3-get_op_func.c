cfbb84496126664e7a25771a3f27de3323766320
Ryn <Ryn@ryn.com>
2026-03-20 23:06:50 +0000

    Lady GAGA

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function for the operator
+ * @s: the operator string
+ *
+ * Return: pointer to the matching function, or NULL if not found.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
